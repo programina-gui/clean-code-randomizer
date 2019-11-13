909d362055f338e55af50e06b900751ec994b352
programina-gui <51759486+programina-gui@users.noreply.github.com>
2019-11-13 14:33:19 +0100

    Project about Clean Code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'clean-code-randomizer';
+  title = 'Learn more about clean code!';
+  cleanCodeArr: string[];
+
+  constructor() {
+    this.cleanCodeArr = [
+      'Name methods with verbs that are meaningful to their action.',
+      'Comments might lie.',
+      'Making code easier to read makes it easier to write (ratio time spent searching vs typing).',
+      'Leave the code cleaner than you found it.',
+      'You are responsible for the quality of your code.'
+    ];
+  }
+
+  showNewLine() {
+    const rand = this.cleanCodeArr[Math.floor(Math.random() *  this.cleanCodeArr.length)];
+    console.log(rand);
+    this.title = rand;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { MatButtonModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
